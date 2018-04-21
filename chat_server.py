be9f8c0451e4ab863dbc96e5b3ceea59b2f998c3
Walter Hill <walterhilldev@gmail.com>
2018-04-21 19:40:24 -0400

    starting work on server
    server takes in client's user and determins whether the user name is valid

--- chat_server.py
+++ chat_server.py
@@ -1,27 +1,48 @@
 import asyncio
 import argparse
 import json
-
+import struct
 
 class ChatServer(asyncio.Protocol):
-
-    def __init__(self):
-        self.username_dict = {}
-        self.messages = {}
-
 
     def connection_made(self, transport):
         self.transport = transport
         self.address = transport.get_extra_info('peername')
         self.data = b''
         print('Accepted connection from {}'.format(self.address))
+        self.user_list = {}
+        self.user_list["USER_LIST"] = []
+        self.messages = {}
+        self.validate_user = {}
+        self.validate_user["USERNAME_ACCEPTED"] = "False"
+
+    def send_message(self, data):
+        byte_count = struct.pack('!I', len(data))
+        self.transport.write(byte_count)
+        self.transport.write(data)
 
     def data_received(self, data):
         self.data += data
-        if self.data.endswith(b'\0'):
-            answer = get_answer(self.data)
-            self.transport.write(answer)
+        print(self.data[0:4])
+        print(self.data[5:])
+
+        if len(self.data[4:]) == struct.unpack('!I', self.data[0:4])[0]:
+            print("all data got")
+            recv_data = json.loads(self.data[4:].decode('ascii'))
+            print(recv_data)
+
+            self.username_check(recv_data['USERNAME'])
+
+            # TODO: Create a single json object to send everything
+            data_json = json.dumps(self.validate_user)
+            # data_json += json.dumps(self.user_list)
+            byte_json = data_json.encode('ascii')
+
+            print(byte_json)
+            self.send_message(byte_json)
+
             self.data = b''
+
 
     def connection_lost(self, exc):
         if exc:
@@ -30,6 +51,17 @@ class ChatServer(asyncio.Protocol):
             print('Client {} sent {} but then closed'.format(self.address, self.data))
         else:
             print('Client {} closed socket'.format(self.address))
+
+    def username_check(self, name):
+
+        if name not in self.user_list["USER_LIST"]:
+            print("Hi new user TRUE")
+            self.user_list["USER_LIST"].append(name)
+            print(self.user_list["USER_LIST"])
+            self.validate_user["USERNAME_ACCEPTED"] = "True"
+        else:
+            self.validate_user["USERNAME_ACCEPTED"] = "False"
+            print("That name is taken: FALSE")
 
 
 def parse_command_line(description):
@@ -45,8 +77,6 @@ def parse_command_line(description):
     args = parser.parse_args()
     address = (args.host, args.p)
     return address
-
-def username_check():
 
 
 if __name__ == '__main__':
